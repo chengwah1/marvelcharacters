88bad416b3a56677add252ece2d83b54221361ff
cheng-wah <chengwah1@hotmail.com>
2019-11-09 20:45:32 +0800

    use spread instead to eliminate redundant name

--- src/Component/Characters/character.js
+++ src/Component/Characters/character.js
@@ -4,16 +4,16 @@ import LikeListContext from '../../LikeListContext';
 
 function Character({name, image, id}) {
 
-    const value = useContext(LikeListContext);
-    const match = value.likeList.findIndex((item)=>item===id)
+    const {likeList,setLikeList} = useContext(LikeListContext); //{likeList,setLikeList}
+    const match = likeList.findIndex((item)=>item===id)
 
     return <div className = "Card">
-                <p>{value.likeList}</p>
+                <p>{likeList}</p>
                 <img src={image} alt="marvel hero" object-fit="fit" width="300" height="300"/>
                 <h1>{name}</h1>
                 <Button displayText={match===-1?"Save to favorite":"Remove from favorite"} onClick={(e)=>{
                 //add to like list
-                value.setLikeList(id,match)
+                setLikeList(id,match)
                 e.stopPropagation()}}/>
             </div>
             }
